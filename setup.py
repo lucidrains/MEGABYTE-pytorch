a067b4ffbbd29c5da1e3008ea15e84ed7fcd6c60
Eric Egli <43848365+eegli@users.noreply.github.com>
2024-09-07 13:43:41 +0200

    revert

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
 
 setup(
   name = 'MEGABYTE-pytorch',
@@ -18,7 +18,7 @@ setup(
   install_requires=[
     'beartype',
     'einops>=0.6.1',
-    'torch>=2',
+    'torch>=1.10',
     'tqdm'
   ],
   classifiers=[
